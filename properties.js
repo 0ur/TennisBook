c71658d57b942aca1679e98d98999182c4be05dc
0ur <7108218@gmail.com>
2023-11-01 18:17:55 +0800

    Update properties.js

--- properties.js
+++ properties.js
@@ -1,1 +1,1 @@
-const google_app_script = "https://script.google.com/macros/s/AKfycbx-DUiIbBeT6k1a0LcH7_xvBlWkE85ErVfj3M_nTGG1_VOJSE_gTmfT6xiFjSy8kW57HA/exec";
+const google_app_script = "https://script.google.com/macros/s/AKfycbx7SZqUp8l_eAOcYqf4b01rjIewp_5tDaINy9rR-kYaqCq_N-DDBgY3gHNZ8XbR8bx7yA/exec";
